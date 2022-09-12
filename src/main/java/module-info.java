c8ece3f111f4462bdf1f4a64e9ade9d53025a9d0
asseemm <assem.shimbergenova@jihc.kz>
2022-09-12 12:52:27 +0600

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.hellofx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.hellofx to javafx.fxml;
+    exports com.example.hellofx;
+}
